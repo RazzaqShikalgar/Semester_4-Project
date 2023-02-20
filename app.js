a705fc90917599ba60bfa04aa4517fbd02776b62
Razzaq Shikalgar <ayubshikalgar@gmail.com>
2023-02-21 00:24:13 +0530

    Add gitignore, procfile and updare ports

--- app.js
+++ app.js
@@ -85,7 +85,11 @@ app.post("/login", function(req,res){
 // -----------Card  Data------------
 
 // --------------------------------------------------
-app.listen(3000, function() {
+let port   = process.env.PORT;
+if(port==null || port==""){
+  port=3000;
+}
+app.listen(port , function() {
   console.log("Server started on port 3000.");
 });
 
